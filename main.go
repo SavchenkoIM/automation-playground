416984950c33b97b0dd37061605ff75e0a3ac469
SavchenkoIM <savch.ivan@gmail.com>
2024-06-10 18:35:32 +0300

    m

--- main.go
+++ main.go
@@ -2,14 +2,20 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"github.com/jackc/pgx/v5/pgxpool"
+	"os"
 )
 
 func main() {
 
 	pool, _ := pgxpool.New(context.Background(), "postgresql://localhost:5432/postgres?user=postgres&password=postgres")
 
-	pool.Query(context.Background(), "SELECT * from information_schema.tables")
+	_, err := pool.Query(context.Background(), "SELECT * from information_schema.tables")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(2)
+	}
 
 	println("Hello World")
 }
